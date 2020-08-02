4d2e93905d51a01dae2ff71da7205776a4e321f8
strahinja <strahinjaa.va@gmail.com>
2020-08-02 13:41:26 +0200

    add info route

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -28,5 +28,8 @@ const persons = [
   
 app.get('/api/persons', (request, response) => response.json(persons) )
 
+app.get('/info', (request, response) => response.send(`Phonebook has info for ${persons.length} people
+<br><br>
+${Date()}`))
 
 app.listen(PORT, () => console.log('app listening on port ', PORT))
