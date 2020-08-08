d3ad35023038d8cd03684ffa88842bdc748f1d65
strahinja <strahinjaa.va@gmail.com>
2020-08-08 13:55:27 +0200

    fix toJSON typo

--- phonebook-backend/models/persons.js
+++ phonebook-backend/models/persons.js
@@ -10,14 +10,13 @@ const personSchema = mongoose.Schema({
     number: String
 })
 
-personSchema.set('ToJSON', {
+personSchema.set('toJSON', {
     transform: (document, returnedObject) =>{
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
     }
 })
-
 
 
 module.exports = mongoose.model('Person', personSchema)
